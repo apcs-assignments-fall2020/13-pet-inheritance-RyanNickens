2e21cb577b18e72a435a9b31f348cc6c4d9d9867
RyanNickens <22rnickens@athenian.org>
2020-11-03 02:25:30 +0000

    passes tests, used super method like we did in class

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,43 @@
 public class Dog extends Pet {
     // Instance variable(s)
+    private String breed;
 
 
     // Constructors
+    public Dog(String a, int b, String c) {
+        super(a, b);
+        this.breed = c;
+    }
+
+    public Dog() {
+        super("jack", 4);
+        this.breed = "Cane Corso";
+    }
     
 
     // makeNoise() method
+    public void makeNoise() {
+        System.out.println("Bark!");
+    }
 
 
     // toString method
+    public String toString() {
+        String str =  "Breed: " + breed;
+        return str;
+    }
 
 
     // Getter
+    public String getBreed() {
+        return this.breed;
+    }
 
 
     // Setter
+    public void setBreed(String breed) {
+        if (breed.trim().length() != 0) {
+            this.breed = breed;
+        } 
+    }
 }
